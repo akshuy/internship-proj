110390bfbc9e6988e4783a64474e01eaf5c5c5ff
Akshay <akshayshama3@gmail.com>
2022-09-28 00:02:28 +0530

    use if ,else,loop conditons

--- wscubetech/wscubetech/views.py
+++ wscubetech/wscubetech/views.py
@@ -10,4 +10,14 @@ def course(request):
 def coursedetail(request,courseid):
     return HttpResponse(courseid)
 def homepage(request):
-    return render(request,"index.html")
+    data = {
+        'title': 'HomepagNew',
+        'bdata' :"welcome to our website",
+        'clist' : ['php','java','django'],
+        'numbers' : [10,20,30,40,50],
+        'student_detail' : [
+            {'name' : 'akshay' , 'phone' : 6378176772 },
+            {'name' : 'ajay' , 'phone' : 9694684245}
+        ]
+    }
+    return render(request,"index.html",data)
