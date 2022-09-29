c9817668b9d455ced2fed141249ab8ee30e593bb
Akshay <akshayshama3@gmail.com>
2022-09-29 22:26:21 +0530

    add python file

--- iblog/iblog/urls.py
+++ iblog/iblog/urls.py
@@ -1,4 +1,4 @@
-"""wscubetech URL Configuration
+"""iblog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,14 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from wscubetech import views
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('aboutUS/',views.aboutUS),
-    path('course/',views.course),
-    path('coursedetail/<courseid>',views.coursedetail),
-    path('',views.homepage),
 ]
